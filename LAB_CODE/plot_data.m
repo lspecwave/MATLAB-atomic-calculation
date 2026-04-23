3a318b831121b6b0900a88ccf9a70f798f930fb3
lspecwave <lspecwave@mail.ustc.edu.cn>
2026-04-23 10:59:14 +0800

    0423

--- LAB_CODE/plot_data.m
+++ LAB_CODE/plot_data.m
@@ -4,7 +4,7 @@ colors=npg(10); % Ten basic colors
 
 %% Input Area
 
-maindir=['\\ARTEMIS-PC\Data\2026-04-21' ...
+maindir=['\\ARTEMIS-PC\Data\2026-04-22' ...
     '\'];%数据来源路径
 
 plotnumber = 1; %是否画原子数
@@ -23,8 +23,8 @@ dual_species = 1; %是否有两种同位素
 normalized_detection = 0; %是否归一化探测，默认OD_2/OD_1
 differential_detection = 1; %是否差分探测
 
-first = 519; %第一个文件夹序号
-last = 670; %最后一个文件夹序号
+first = 588; %第一个文件夹序号
+last = 667; %最后一个文件夹序号
 
 %设置横坐标公式为: xaxis=(first-1:last-1)*coeff+intercept;
 intercept = 0; %第一组数据的自变量`
